75fba7187cccdf844d46b45047da3d9526c1ebcd
Yves286 <140513871+Yves286@users.noreply.github.com>
2024-11-20 17:56:10 +1100

    list files of desired extension

--- auto_file_remover.py
+++ auto_file_remover.py
@@ -1,8 +1,16 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 load_dotenv()
 
 source_path = os.getenv('SOURCE_PATH')
 print(source_path)
 isExist = os.path.exists(source_path)
-print(isExist)
+#print(isExist)
+
+# get list of all files
+arrFile = os.listdir(source_path)
+#print(arrFile)
+for i in arrFile:
+    if (Path(i).suffix == ('.mp3')) or (Path(i).suffix == ('.wav')): # get desired file extensions
+        print(i)
