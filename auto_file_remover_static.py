dd7ec2147cf70f47bd9a5f7b6c53ee13a8b1c924
Yves286 <140513871+Yves286@users.noreply.github.com>
2024-11-21 16:48:38 +1100

    rename file + add terminations

--- auto_file_remover_static.py
+++ auto_file_remover_static.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 from dotenv import load_dotenv
 from pathlib import Path
 load_dotenv()
@@ -9,9 +11,15 @@ dest_path = os.getenv('DEST_PATH')
 # get list of all files
 arrFile = os.listdir(source_path)
 #print(arrFile)
+if (len(arrFile) <= 0):
+    print("No files in source location. Program ending.")
+    sys.exit(0)
+
 for i in arrFile:
     if (Path(i).suffix == ('.mp3')) or (Path(i).suffix == ('.wav')): # get desired file extensions
         src_path = os.path.join(source_path, i)
         dst_path = os.path.join(dest_path, i)
         os.rename(src_path, dst_path)
         print(i + ' has been moved')
+    print("All files moved. Program ending.")
+    sys.exit(0)
